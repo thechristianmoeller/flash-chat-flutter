5943950600cb9195ca3efffd21433b774f581058
thechristianmoeller <72333493+thechristianmoeller@users.noreply.github.com>
2022-11-26 22:41:14 +0900

    Added initial Firebase support
    Added initial Firebase support for iOS and Android.
    
    Refactored application to use "com.thechristianmoeller.flash_chat" and "com.thechristianmoeller.flashChat" for Android and iOS, respectively.
    
    **NOTE:
    Had to open xCode and remove the flutter-ios-sdk dependency to successfully complete xCode build.

---
+++ android/app/src/main/java/com/thechristianmoeller/flash_chat/MainActivity.java
@@ -0,0 +1,40 @@
+package com.thechristianmoeller.flash_chat;
+//package com.thechristianmoeller.practice1_flutter3;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+
+public class MainActivity extends FlutterActivity {
+
+//    super.onCreate(savedInstanceState);
+//    GeneratedPluginRegistrant.registerWith(this);
+
+//    @Override
+//    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+//        GeneratedPluginRegistrant.registerWith(flutterEngine);
+//        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler(
+//                (call, result) -> {
+//
+//                }
+//        )
+//    }
+}
+
+//package com.thechristian.flash_chat;
+//
+//import android.os.Bundle;
+//import io.flutter.app.FlutterActivity;
+//import io.flutter.plugins.GeneratedPluginRegistrant;
+//import io.flutter.embedding.android.FlutterActivity;
+//
+//public class MainActivity extends FlutterActivity {
+//  @Override
+//  protected void onCreate(Bundle savedInstanceState) {
+//    super.onCreate(savedInstanceState);
+//    GeneratedPluginRegistrant.registerWith(this);
+//  }
--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,15 @@ import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 
-void main() => runApp(FlashChat());
+// void main() => runApp(const FlashChat());
+
+void main() {
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,15 +22,14 @@ class FlashChat extends StatelessWidget {
       //   ),
       // ),
       initialRoute: WelcomeScreen.id,
-        routes: {
-          WelcomeScreen.id: (context) => WelcomeScreen(),
-          LoginScreen.id: (context) => LoginScreen(),
-          RegistrationScreen.id: (context) => RegistrationScreen(),
-          ChatScreen.id: (context) => ChatScreen(),
-        },
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
 
       // home: WelcomeScreen(),
-
     );
   }
 }
